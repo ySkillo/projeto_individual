4fd5dfbd91b5131bc87664f7759f8fdc2e0afa0c
yskillo <matheus.doliveira@sptech.school>
2025-05-02 20:15:31 -0300

    tela de login feita

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,5 +1,17 @@
 var database = require("../database/config")
 
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT idUsuario, nomeUsuario, emailUsuario, cpfUsuario FROM tbUsuario WHERE emailUsuario = '${email}' AND senhaUsuario = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(nome, email, senha, cpf) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf);
@@ -14,5 +26,6 @@ function cadastrar(nome, email, senha, cpf) {
 }
 
 module.exports = {
+    autenticar,
     cadastrar
 };
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -8,5 +8,9 @@ router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
 })
 
+router.post("/autenticar", function (req, res){
+    usuarioController.autenticar(req, res);
+})
+
 
 module.exports = router;
