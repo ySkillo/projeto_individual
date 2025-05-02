efaa53e714af9838ec44e417b5c687dec95a4196
yskillo <matheus.doliveira@sptech.school>
2025-05-02 19:13:36 -0300

    alterações no diretório

---
+++ src/models/usuarioModel.js
@@ -0,0 +1,18 @@
+var database = require("../database/config")
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nome, email, senha, cpf) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cpf);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO tbUsuario (nomeUsuario, nomePerfilUsuario, emailUsuario, senhaUsuario, cpfUsuario, fotoPerfil) VALUES ('${nome}','${null}', '${email}', '${senha}', '${cpf}', '${null}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    cadastrar
+};
